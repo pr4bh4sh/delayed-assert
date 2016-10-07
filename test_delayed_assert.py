d4f0c98cb8642c206391272dc6995064a6a97dd6
Prabhash Singh <pr4bh4sh@users.noreply.github.com>
2016-10-07 16:57:35 +0530

    Create test_delayed_assert.py

---
+++ test_delayed_assert.py
@@ -0,0 +1,16 @@
+from delayed_assert import expect, assert_expectations
+import unittest
+ 
+class DelayedAssertTest(unittest.TestCase):
+ 
+    def test_should_pass(self):
+        expect(1 == 1, 'one is one')
+        assert_expectations()
+ 
+    def test_should_fail(self):
+        expect(1 == 2)
+        x = 1
+        y = 2
+        expect(x == y, 'x:%s y:%s' % (x,y))
+        expect(1 == 1)
+        assert_expectations()
