4dff1249ad1ea3134f5d61fbc14b4631e232d1d8
Aaron Bannin <aaronbannin@gmail.com>
2017-01-24 22:36:46 -0800

    implement unittest

--- delayed_assert/test_delayed_assert.py
+++ delayed_assert/test_delayed_assert.py
@@ -1,9 +1,9 @@
 # from ..delayed_assert import expect, assert_expectations
-
+import unittest
 from delayed_assert import expect, assert_expectations
 
 
-class TestDelayedAssert:
+class TestDelayedAssert(unittest.TestCase):
  
     def test_should_pass(self):
         expect(1 == 1, 'one is one')
