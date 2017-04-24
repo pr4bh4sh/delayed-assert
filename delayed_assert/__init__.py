45c7e910f13a43427359801782eef7ce537d6f5f
Prabhash Singh <pr4bh4sh@users.noreply.github.com>
2017-04-24 23:56:09 +0530

    Support for python 2 and 3

--- delayed_assert/__init__.py
+++ delayed_assert/__init__.py
@@ -1,1 +1,6 @@
-from delayed_assert.delayed_assert import expect, assert_expectations
+import sys
+
+if sys.version_info > (3, 0):  # Python 3 and above
+    from delayed_assert.delayed_assert import expect, assert_expectations
+else:  # for Python 2 
+    from delayed_assert import expect, assert_expectations
