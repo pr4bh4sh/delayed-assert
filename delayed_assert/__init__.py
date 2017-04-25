541e3f9c07bc1755bfb91621b5312736fafe513f
Prabhash Singh <pr4bh4sh@users.noreply.github.com>
2017-04-25 19:32:46 +0530

    made it relative import, no need to check version

--- delayed_assert/__init__.py
+++ delayed_assert/__init__.py
@@ -1,6 +1,1 @@
-import sys
-
-if sys.version_info > (3, 0):  # Python 3 and above
-    from delayed_assert.delayed_assert import expect, assert_expectations
-else:  # for Python 2 
-    from delayed_assert import expect, assert_expectations
+from .delayed_assert import expect, assert_expectations
