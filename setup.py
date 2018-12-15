c878abb7b7afd10e6fdc7972364718a913f30071
Prabhash Singh <prabhash.de@gmail.com>
2018-12-15 23:12:18 +0000

    Fixing project discription for pypi

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup
 
 setup(
     name='delayed_assert',
-    version='0.2',
+    version='0.2.1',
     description='Delayed/soft assertions for python',
+    long_description = 'Project description and details at https://github.com/pr4bh4sh/python-delayed-assert',
     author='pr4bh4sh, aaronbannin',
     url='https://github.com/pr4bh4sh/python-delayed-assert',
     packages=['delayed_assert'],
-    zip_safe=False
 )
