4bdd62ed2fccab300eaaf1423b950801254f7e5d
Prabhash Singh <prabhash.de@gmail.com>
2018-12-18 23:50:16 +0000

    Bumping the version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='delayed_assert',
-    version='0.2.1',
+    version='0.2.3',
     description='Delayed/soft assertions for python',
     long_description = 'Project description and details at https://github.com/pr4bh4sh/python-delayed-assert',
     author='pr4bh4sh, aaronbannin',
