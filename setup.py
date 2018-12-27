5cf54b47d6a1f91d221f985c085482c748ce4c4a
Prabhash Singh <prabhash.de@gmail.com>
2018-12-27 00:52:26 +0000

    Pip installation fix

--- setup.py
+++ setup.py
@@ -1,15 +1,13 @@
+import os
+import io
 from setuptools import setup
 
-from os import path
-this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
 
 setup(
     name='delayed_assert',
-    version='0.3.1',
+    version='0.3.2',
     description='Delayed/soft assertions for python',
-    long_description=long_description,
+    long_description=io.open(os.path.join(os.path.dirname('__file__'), 'README.md'), encoding='utf-8').read(),
     long_description_content_type='text/markdown',
     author='pr4bh4sh',
     url='https://github.com/pr4bh4sh/python-delayed-assert',
