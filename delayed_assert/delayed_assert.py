fa4bc63e057c71608ee8599aa97bc081a44d7f3a
Prabhash <pr4bh4sh@users.noreply.github.com>
2025-12-07 21:38:04 +0000

    Fix CI: remove unused global and update Python versions

--- delayed_assert/delayed_assert.py
+++ delayed_assert/delayed_assert.py
@@ -135,7 +135,7 @@ def _report_failures():
 
 def expect(expr, msg=None):
     """Keep track of failed expectations."""
-    global _failed_expectations, _is_first_call
+    global _failed_expectations
     caller = ''
 
     # Ensure that the call is coming from 'test*' method
